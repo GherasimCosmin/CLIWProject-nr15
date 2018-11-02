b6b2f6ca491067d5b4fefd0444b58bfccea3c1ae
GherasimCosmin <gherasimcosmin97@yahoo.com>
2018-11-02 06:51:16 +0200

    offfffff

--- javascriptFiles/swiper.js
+++ javascriptFiles/swiper.js
@@ -23,6 +23,7 @@ marginTopRandom =window.getComputedStyle(random).getPropertyValue('margin-top');
 
 
 window.onload = function(){
+  setup();
   position=slide[1];
   document.querySelector(".swiper__list").style.display="none";
   document.querySelector(".swiper__generate").style.display="block";
@@ -159,4 +160,34 @@ function right(){
   document.querySelector(".swiper__random").style.display="none";
 
   document.querySelector(classToBe).style.display='block';
+}
+var canvas,drawobj;
+function setup()
+{
+  canvas=document.getElementById("thecanvas");
+  drawobj = canvas.getContext("2d");
+  drawobj.translate(0.5, 0.5);
+  cantor(canvas.width/2,canvas.height/2,500,0);
+}
+function cantor(x, y,len,flag){
+  if(len>=1){
+    if(flag==0){
+      drawline(x,y,x+len,y);
+      drawline(x,y,x-len,y);
+      cantor(x+len,y,len*0.67,1);
+      cantor(x-len,y,len*0.67,1);
+    }
+    else{
+      drawline(x,y,x,y+len);
+      drawline(x,y,x,y-len);
+      cantor(x,y+len,len*0.67,0);
+      cantor(x,y-len,len*0.67,0);
+    }
+  }
+}
+function drawline(x,y,x2,y2){
+  drawobj.beginPath();
+  drawobj.moveTo(x,y);
+  drawobj.lineTo(x2,y2);
+  drawobj.stroke();
 }
