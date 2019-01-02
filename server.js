583e33a4ecdaac5fbcd4bee613e6d8bef21e1935
GherasimCosmin <gherasimcosmin97@yahoo.com>
2019-01-02 19:43:54 +0200

    restructurare

--- server.js
+++ server.js
@@ -10,12 +10,10 @@ let con = mysql.createConnection({
 	database: "lsystems"
 });
 
-// This responds with "Hello World" on the homepage
 app.get('/', function(req, res) {
-  // do something here.
-  res.send('Hello POST');
+	res.send('Hello Cosmin');
 });
-// login route
+
 app.get('/index', function (req, res) {
 	console.log("Got a GET request for the homepage");
 	response = {
@@ -25,18 +23,22 @@ app.get('/index', function (req, res) {
 	console.log(response);
 	res.end(JSON.stringify(response));
 })
-// sing up route
+
+// sign up route
 app.get('/sign', function (req, res) {
-	console.log("signUP req");
-		response = {
+	response = {
 		username:req.query.username,
 		password:req.query.password
 	};
-		con.connect(function(err) {
-		if (err) throw err;
+	con.connect(function(err) {
+		
+		if (err)
+			throw err;
 		console.log("Connected!");
+		
 		var sql = "INSERT INTO users (username, password) VALUES ('"
 		+response.username+"', '"+response.password+"')";
+		
 		con.query(sql, function (err, result) {
 			if (err) throw err;
 			console.log("1 record inserted");
@@ -58,15 +60,10 @@ app.get('/list_user', function (req, res) {
 	res.send('Page Listing');
 })
 
-// This responds a GET request for abcd, abxcd, ab123cd, and so on
-app.get('/ab*cd', function(req, res) {   
-	console.log("Got a GET request for /ab*cd");
-	res.send('Page Pattern Match');
-})
-
 var server = app.listen(8085, function () {
 	var host = server.address().address
 	var port = server.address().port
 
 	console.log("Example app listening at http://%s:%s", host, port)
 })
+//node server.js
